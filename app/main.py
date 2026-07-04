5867e10d4a6ecdd10edd2dd78c14244b8fbb914a
Darshan <your.email@example.com>
2026-07-05 04:23:17 +0530

    DD-26 Add CSS styling

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
 from app.routers import pnr
 
 
@@ -8,9 +12,21 @@ app = FastAPI(
     description = "Backend API for Railway PNR Status Checker"
 )
 
+#Serve static files
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+#templates
+templates = Jinja2Templates(directory="app/templates")
+
+#Home Page
 @app.get("/")
-def home():
-    return {
-        "message" : "Welcome to Railway PNR Status Checker API 🚆"
-    }
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"request": request},
+    )
+    
+
+ #API Router   
 app.include_router(pnr.router)
---
+++ app/static/js/app.js
@@ -0,0 +1,1 @@
+console.log("Railway PNR UI Loaded 🚆");
